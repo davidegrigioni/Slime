64ed6d35b899c173bcd4c77e6ef4de76eaf53ae8
davide <dav.grigioni@gmail.com>
2024-09-13 03:19:08 +0200

    fixed interaction

--- src/main/java/cc/davyy/slime/listeners/PlayerInteractListener.java
+++ src/main/java/cc/davyy/slime/listeners/PlayerInteractListener.java
@@ -2,8 +2,6 @@ package cc.davyy.slime.listeners;
 
 import cc.davyy.slime.gui.ServerGUI;
 import cc.davyy.slime.model.SlimePlayer;
-import cc.davyy.slime.utils.TagConstants;
-import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.event.player.PlayerUseItemEvent;
 import net.minestom.server.item.ItemStack;
@@ -21,9 +19,6 @@ public class PlayerInteractListener implements EventListener<PlayerUseItemEvent>
     public @NotNull Result run(@NotNull PlayerUseItemEvent event) {
         final SlimePlayer player = (SlimePlayer) event.getPlayer();
         final ItemStack item = event.getItemStack();
-        final Player.Hand hand = event.getHand();
-
-        if (hand != Player.Hand.MAIN) return Result.INVALID;
 
         switch (item.getTag(Tag.String("action"))) {
             case "lobbysl" -> new ServerGUI().open(player);
