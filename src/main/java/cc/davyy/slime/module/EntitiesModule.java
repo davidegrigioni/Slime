16093bf0eccfc7fc732b7865e1b2b5eb3d265847
davide <dav.grigioni@gmail.com>
2024-09-19 19:08:50 +0200

    implemented tags from msntags

--- src/main/java/cc/davyy/slime/module/EntitiesModule.java
+++ src/main/java/cc/davyy/slime/module/EntitiesModule.java
@@ -3,10 +3,13 @@ package cc.davyy.slime.module;
 import cc.davyy.slime.commands.entities.HologramCommand;
 import cc.davyy.slime.commands.entities.NPCCommand;
 import cc.davyy.slime.managers.entities.HologramManager;
-import cc.davyy.slime.managers.entities.NPCManager;
+import cc.davyy.slime.managers.entities.npc.NPCManager;
 import cc.davyy.slime.services.entities.HologramService;
-import cc.davyy.slime.services.entities.NPCService;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import net.minestom.server.event.Event;
+import net.minestom.server.event.EventNode;
 
 public class EntitiesModule extends AbstractModule {
 
@@ -15,11 +18,16 @@ public class EntitiesModule extends AbstractModule {
         bind(HologramManager.class);
         bind(NPCManager.class);
 
-        bind(NPCService.class).to(NPCManager.class);
         bind(HologramService.class).to(HologramManager.class);
 
         bind(HologramCommand.class);
         bind(NPCCommand.class);
+    }
+
+    @Provides
+    @Singleton
+    public EventNode<Event> provideEventNode() {
+        return EventNode.all("npc-events");
     }
 
 }
