913bb3666e95324c8edf89b0c42bc24df930b4f9
davide <dav.grigioni@gmail.com>
2024-09-19 19:09:07 +0200

    hub command init (should teleport to main lobby)

---
+++ src/main/java/cc/davyy/slime/commands/player/HubCommand.java
@@ -0,0 +1,12 @@
+package cc.davyy.slime.commands.player;
+
+import net.minestom.server.command.builder.Command;
+import org.jetbrains.annotations.NotNull;
+
+public class HubCommand extends Command {
+
+    public HubCommand() {
+        super("hub");
+    }
+
+}
