5a0369f8afb507eb5260e683dc20ad77abe10ac7
davide <dav.grigioni@gmail.com>
2024-09-20 04:31:46 +0200

    configurable socials

--- src/main/java/cc/davyy/slime/commands/player/SocialCommand.java
+++ src/main/java/cc/davyy/slime/commands/player/SocialCommand.java
@@ -1,16 +1,15 @@
 package cc.davyy.slime.commands.player;
 
 import com.google.inject.Singleton;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
 import org.jetbrains.annotations.NotNull;
 
-import static net.kyori.adventure.text.Component.newline;
-import static net.kyori.adventure.text.Component.text;
+import java.util.List;
+
+import static cc.davyy.slime.utils.FileUtils.getConfig;
+import static cc.davyy.slime.utils.ColorUtils.of;
 
 @Singleton
 public class SocialCommand extends Command {
@@ -22,40 +21,11 @@ public class SocialCommand extends Command {
     }
 
     private void execute(@NotNull CommandSender sender, @NotNull CommandContext context) {
-        final Component twitterComponent = text("Twitter: ")
-                .color(TextColor.fromHexString("#1DA1F2"))  // Twitter's blue color
-                .append(text("@YourTwitterHandle")
-                        .color(TextColor.fromHexString("#1DA1F2"))
-                        .decorate(TextDecoration.BOLD));
+        final List<String> socials = getConfig().getStringList("socials");
+        final String socialMessage = String.join("\n", socials);
 
-        final Component tiktokComponent = text("TikTok: ")
-                .color(TextColor.fromHexString("#000000"))  // TikTok's black color
-                .append(text("@YourTikTokHandle")
-                        .color(TextColor.fromHexString("#00F2F3"))  // TikTok's gradient color
-                        .decorate(TextDecoration.BOLD));
-
-        final Component instagramComponent = text("Instagram: ")
-                .color(TextColor.fromHexString("#C13584"))  // Instagram's pink color
-                .append(text("@YourInstagramHandle")
-                        .color(TextColor.fromHexString("#C13584"))
-                        .decorate(TextDecoration.BOLD));
-
-        final Component youtubeComponent = text("YouTube: ")
-                .color(TextColor.fromHexString("#FF0000"))  // YouTube's red color
-                .append(text("YourYouTubeChannelURL")
-                        .color(TextColor.fromHexString("#FF0000"))
-                        .decorate(TextDecoration.BOLD));
-
-        final Component socialMessage = text("Follow us on social media:\n")
-                .append(twitterComponent)
-                .append(newline())
-                .append(tiktokComponent)
-                .append(newline())
-                .append(instagramComponent)
-                .append(newline())
-                .append(youtubeComponent);
-
-        sender.sendMessage(socialMessage);
+        sender.sendMessage(of(socialMessage)
+                .build());
     }
 
 }
