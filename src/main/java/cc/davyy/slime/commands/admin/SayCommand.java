29a1012b7f56ee22324ec90c08d3fd51f748f9a6
davide <dav.grigioni@gmail.com>
2024-09-20 20:15:26 +0200

    adjusted saycommand

--- src/main/java/cc/davyy/slime/commands/admin/SayCommand.java
+++ src/main/java/cc/davyy/slime/commands/admin/SayCommand.java
@@ -3,14 +3,12 @@ package cc.davyy.slime.commands.admin;
 import cc.davyy.slime.model.SlimePlayer;
 import cc.davyy.slime.utils.Messages;
 import com.google.inject.Singleton;
-import net.kyori.adventure.text.Component;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
 import net.minestom.server.command.builder.arguments.ArgumentStringArray;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.minecraft.ArgumentEntity;
-import net.minestom.server.utils.entity.EntityFinder;
 import org.jetbrains.annotations.NotNull;
 
 import static cc.davyy.slime.utils.GeneralUtils.hasPlayerPermission;
@@ -37,11 +35,10 @@ public class SayCommand extends Command {
 
     private void execute(@NotNull CommandSender sender, @NotNull CommandContext context) {
         final SlimePlayer player = (SlimePlayer) sender;
-        final EntityFinder finder = context.get(playerArg);
-        final SlimePlayer target = (SlimePlayer) finder.findFirstPlayer(player);
+        final SlimePlayer target = (SlimePlayer) context.get(playerArg).findFirstPlayer(player);
         final String finalMessage = String.join(" ", context.get(messageArray));
 
-        target.sendMessage(target.getChatFormat(finalMessage) + finalMessage);
+        target.sendMessage(target.getChatFormat(finalMessage));
     }
 
 }
