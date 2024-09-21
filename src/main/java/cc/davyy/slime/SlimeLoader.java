7df682d0d3aaf36237eb7e990a513d84cc1cdbc1
davide <dav.grigioni@gmail.com>
2024-09-21 19:34:13 +0200

    Adjusted CommandManager

--- src/main/java/cc/davyy/slime/SlimeLoader.java
+++ src/main/java/cc/davyy/slime/SlimeLoader.java
@@ -1,15 +1,5 @@
 package cc.davyy.slime;
 
-import cc.davyy.slime.commands.ItemDisplayCommand;
-import cc.davyy.slime.commands.VehicleCommand;
-import cc.davyy.slime.commands.admin.*;
-import cc.davyy.slime.commands.cosmetic.CosmeticCommand;
-import cc.davyy.slime.commands.entities.HologramCommand;
-import cc.davyy.slime.commands.entities.NPCCommand;
-import cc.davyy.slime.commands.player.SidebarCommand;
-import cc.davyy.slime.commands.player.SocialCommand;
-import cc.davyy.slime.commands.player.SpawnCommand;
-import cc.davyy.slime.commands.player.TeleportCommand;
 import cc.davyy.slime.listeners.*;
 import cc.davyy.slime.managers.*;
 import cc.davyy.slime.managers.entities.HologramManager;
@@ -38,6 +28,7 @@ public class SlimeLoader {
 
     private ServerMode serverMode;
 
+    @Inject private CommandManager commandManager;
     @Inject private BroadcastManager broadcastManager;
     @Inject private BrandManager brandManager;
     @Inject private LobbyManager lobbyManager;
@@ -46,25 +37,9 @@ public class SlimeLoader {
     @Inject private SpawnManager spawnManager;
     @Inject private GameModeManager gameModeManager;
 
-    @Inject private BroadCastCommand broadcastCommand;
-    @Inject private ConfigReloadCommand configReloadCommand;
-    @Inject private CosmeticCommand cosmeticCommand;
-    @Inject private ExecuteCommand executeCommand;
-    @Inject private GameModeCommand gameModeCommand;
-    @Inject private HologramCommand hologramCommand;
-    @Inject private LobbyCommand lobbyCommand;
-    @Inject private NPCCommand npcCommand;
-    @Inject private SayCommand sayCommand;
-    @Inject private SocialCommand socialCommand;
-    @Inject private SpawnCommand spawnCommand;
-    @Inject private StopCommand stopCommand;
-    @Inject private TeleportCommand teleportCommand;
-    @Inject private SidebarCommand sidebarCommand;
-    @Inject private VehicleCommand vehicleCommand;
-
     @Inject private EventsListener eventsListener;
 
-    public void start() {
+    public void init() {
         LOGGER.info("Initializing Minecraft Server...");
         final MinecraftServer minecraftServer = MinecraftServer.init();
 
@@ -78,7 +53,7 @@ public class SlimeLoader {
         ConsoleUtils.setupConsole();
 
         LOGGER.info("Registering commands...");
-        registerCommands();
+        commandManager.init();
 
         LOGGER.info("Setting up shutdown tasks...");
         setupShutdownTask();
@@ -94,27 +69,6 @@ public class SlimeLoader {
         LOGGER.info("Injecting dependencies using Guice...");
         final Injector injector = Guice.createInjector(new SlimeModule());
         injector.injectMembers(this);
-    }
-
-    private void registerCommands() {
-        final var commandManager = MinecraftServer.getCommandManager();
-
-        commandManager.register(broadcastCommand,
-                configReloadCommand,
-                cosmeticCommand,
-                executeCommand,
-                gameModeCommand,
-                hologramCommand,
-                lobbyCommand,
-                npcCommand,
-                sayCommand,
-                socialCommand,
-                spawnCommand,
-                stopCommand,
-                sidebarCommand,
-                vehicleCommand,
-                new ItemDisplayCommand(),
-                teleportCommand);
     }
 
     private void setupShutdownTask() {
---
+++ src/main/java/cc/davyy/slime/managers/CommandManager.java
@@ -0,0 +1,57 @@
+package cc.davyy.slime.managers;
+
+import cc.davyy.slime.commands.ItemDisplayCommand;
+import cc.davyy.slime.commands.VehicleCommand;
+import cc.davyy.slime.commands.admin.*;
+import cc.davyy.slime.commands.cosmetic.CosmeticCommand;
+import cc.davyy.slime.commands.entities.HologramCommand;
+import cc.davyy.slime.commands.entities.NPCCommand;
+import cc.davyy.slime.commands.player.SidebarCommand;
+import cc.davyy.slime.commands.player.SocialCommand;
+import cc.davyy.slime.commands.player.SpawnCommand;
+import cc.davyy.slime.commands.player.TeleportCommand;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import net.minestom.server.MinecraftServer;
+
+@Singleton
+public class CommandManager {
+
+    @Inject private BroadCastCommand broadcastCommand;
+    @Inject private ConfigReloadCommand configReloadCommand;
+    @Inject private CosmeticCommand cosmeticCommand;
+    @Inject private ExecuteCommand executeCommand;
+    @Inject private GameModeCommand gameModeCommand;
+    @Inject private HologramCommand hologramCommand;
+    @Inject private LobbyCommand lobbyCommand;
+    @Inject private NPCCommand npcCommand;
+    @Inject private SayCommand sayCommand;
+    @Inject private SocialCommand socialCommand;
+    @Inject private SpawnCommand spawnCommand;
+    @Inject private StopCommand stopCommand;
+    @Inject private TeleportCommand teleportCommand;
+    @Inject private SidebarCommand sidebarCommand;
+    @Inject private VehicleCommand vehicleCommand;
+
+    public void init() {
+        final var commandRegistry = MinecraftServer.getCommandManager();
+
+        commandRegistry.register(broadcastCommand,
+                configReloadCommand,
+                cosmeticCommand,
+                executeCommand,
+                gameModeCommand,
+                hologramCommand,
+                lobbyCommand,
+                npcCommand,
+                sayCommand,
+                socialCommand,
+                spawnCommand,
+                stopCommand,
+                sidebarCommand,
+                vehicleCommand,
+                new ItemDisplayCommand(),
+                teleportCommand);
+    }
+
+}
